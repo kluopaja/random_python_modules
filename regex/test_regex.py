a22571a6c6487b34cf91cc0081d79a07bdd1f26e
kluopaja <kalle.luopajarvi@helsinki.fi>
2020-08-25 12:47:28 +0300

    Add unit tests for regex
        -Add unit tests for process_concatenation
        -Add unit tests for process_unary

--- regex/test_regex.py
+++ regex/test_regex.py
@@ -194,5 +194,88 @@ class TestRegexParsing(unittest.TestCase):
         with self.assertRaises(Exception):
             regex.parse_union(tmp)
 
+    def test_process_concatenation(self):
+        a = regex.process_concatenation([regex.ParseTreeNode(normal='a')])
+        b = [regex.ParseTreeNode(normal='a')]
+        self.assertEqual(a, b)
+
+        tmp = [regex.ParseTreeNode(normal='a'),
+               regex.ParseTreeNode(normal='a')]
+        a = regex.process_concatenation(tmp)
+        c1 = regex.ParseTreeNode(normal='a')
+        c2 = regex.ParseTreeNode(normal='a')
+        b = regex.ParseTreeNode(children=[c1, c2], operation='concatenation')
+        b = [b]
+        self.assertEqual(a, b)
+
+        tmp = [regex.ParseTreeNode(normal='a'),
+               regex.ParseTreeNode(normal='b'),
+               regex.ParseTreeNode(normal='c')]
+        a = regex.process_concatenation(tmp)
+        n1 = regex.ParseTreeNode(normal='a')
+        n2 = regex.ParseTreeNode(normal='b')
+        n3 = regex.ParseTreeNode(normal='c')
+        n4 = regex.ParseTreeNode(children=[n1, n2], operation='concatenation')
+        b = regex.ParseTreeNode(children=[n4, n3], operation='concatenation')
+        b = [b]
+        self.assertEqual(a, b)
+
+        tmp = [regex.ParseTreeNode(normal='a'),
+               regex.ParseTreeNode(normal='b'),
+               regex.ParseTreeNode(meta='|'),
+               regex.ParseTreeNode(normal='c'),
+               regex.ParseTreeNode(normal='d')]
+        a = regex.process_concatenation(tmp)
+        n1 = regex.ParseTreeNode(normal='a')
+        n2 = regex.ParseTreeNode(normal='b')
+        n3 = regex.ParseTreeNode(children=[n1, n2], operation='concatenation')
+        n4 = regex.ParseTreeNode(normal='c')
+        n5 = regex.ParseTreeNode(normal='d')
+        n6 = regex.ParseTreeNode(children=[n4, n5], operation='concatenation')
+        n7 = regex.ParseTreeNode(meta='|')
+        b = [n3, n7, n6]
+        self.assertEqual(a, b)
+
+    def test_process_unary(self):
+        a = regex.process_unary([regex.ParseTreeNode(normal='a')])
+        b = [regex.ParseTreeNode(normal='a')]
+        self.assertEqual(a, b)
+
+        tmp = [regex.ParseTreeNode(normal='a'),
+               regex.ParseTreeNode(meta='*')]
+        a = regex.process_unary(tmp)
+        c1 = regex.ParseTreeNode(normal='a')
+        b = regex.ParseTreeNode(children=[c1], operation='*')
+        b = [b]
+        self.assertEqual(a, b)
+
+        tmp = [regex.ParseTreeNode(meta='*'),
+               regex.ParseTreeNode(normal='a')]
+        with self.assertRaises(ValueError):
+            regex.process_unary(tmp)
+
+        tmp = [regex.ParseTreeNode(meta='|'),
+               regex.ParseTreeNode(meta='*')]
+        with self.assertRaises(ValueError):
+            regex.process_unary(tmp)
+
+        tmp = [regex.ParseTreeNode(normal='a'),
+               regex.ParseTreeNode(meta='+'),
+               regex.ParseTreeNode(normal='b'),
+               regex.ParseTreeNode(meta='?'),
+               regex.ParseTreeNode(meta='|'),
+               regex.ParseTreeNode(normal='c'),
+               regex.ParseTreeNode(meta='*')]
+        a = regex.process_unary(tmp)
+        n1 = regex.ParseTreeNode(normal='a')
+        n2 = regex.ParseTreeNode(children=[n1], operation='+')
+        n3 = regex.ParseTreeNode(normal='b')
+        n4 = regex.ParseTreeNode(children=[n3], operation='?')
+        n5 = regex.ParseTreeNode(meta='|')
+        n6 = regex.ParseTreeNode(normal='c')
+        n7 = regex.ParseTreeNode(children=[n6], operation='*')
+        b = [n2, n4, n5, n7]
+        self.assertEqual(a, b)
+
 if __name__ == '__main__':
     unittest.main()
